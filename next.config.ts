c07f595b753115389a586b9202abebc4f4db69ff
bakhadir <myblogfree1988@gmail.com>
2025-01-29 14:09:07 +0300

    add server file and edit next-config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  /* config options here */
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+  },
 };
 
 export default nextConfig;
