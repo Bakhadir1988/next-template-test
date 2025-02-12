1a4ac3605a5cb2deab1b245c343cbcc726b71e07
Bakhadir1988 <myblogfree1988@gmail.com>
2025-02-12 06:07:56 +0300

    Убрал лишнее

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,13 +21,6 @@ export default function Home() {
           <li>Тест деплоя</li>
           <li>Деплой прошел</li>
           <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
-          <li>Деплой прошел c window</li>
         </ol>
 
         <div className={styles.ctas}>
